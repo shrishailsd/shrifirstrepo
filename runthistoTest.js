9a4713496a2e82bf886dbd9b92ab4e80d860e1c3
sdoddali <sdoddali@cisco.com>
2022-08-30 18:47:58 -0400

    commit port

--- runthistoTest.js
+++ runthistoTest.js
@@ -118,6 +118,6 @@ app.get("/url", (req, res, next) => {
     });
 });
 
-app.listen(3000, () => {
-    console.log("Server running on port 3000");
+app.listen(5000, () => {
+    console.log("Server running on port 5000");
 });
