fa541bbc423f042cb12dd2ed1e58aaf885b2ab16
sdoddali <sdoddali@cisco.com>
2022-09-13 15:36:15 -0400

    new qqm api 11 test

--- runthistoTest.js
+++ runthistoTest.js
@@ -68,7 +68,7 @@ app.set('port', (process.env.PORT || 5000));
 
 //For avoidong Heroku $PORT error
 app.get('/', function(request, response) {
-    var result = 'SHRISHAIL APP changed app'
+    var result = 'MYKOLA'
     response.send(result);
 }).listen(app.get('port'), function() {
     console.log('App is running, server is listening on port ', app.get('port'));
