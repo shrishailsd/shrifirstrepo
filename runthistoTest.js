3b9d1258c6a3c841a826b0c43ee3d077f83f8624
sdoddali <sdoddali@cisco.com>
2022-09-16 14:57:38 -0400

    removed

--- runthistoTest.js
+++ runthistoTest.js
@@ -6,12 +6,7 @@ var app = express();
 
 
 app.get("/url", (req, res, next) => {
-    res.status(500).json({
-        "Code": 500,
-        "Description": "Card not Found",
-        "Id": -1,
-        "Module": "servicesparticipantcards"
-    });
+    res.status(500).json({ hi });
 
 
 });
